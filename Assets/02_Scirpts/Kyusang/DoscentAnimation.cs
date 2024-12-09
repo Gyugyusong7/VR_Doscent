02deb516efdc2a66be2f2cfccd629b09280aaec5
Kyusang <gyugyusong@yonsei.ac.kr>
2024-12-09 10:16:45 +0900

    /

--- Assets/02_Scirpts/Kyusang/DoscentAnimation.cs
+++ Assets/02_Scirpts/Kyusang/DoscentAnimation.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 
 public class DoscentAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Animator animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
