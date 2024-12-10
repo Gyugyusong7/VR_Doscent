e42408409fc7b00631a9fab836daf2b7edf64727
Biitjoelle <biitjoelle.hy@gmail.com>
2024-12-10 12:39:53 +0900

    12/10

--- Assets/02_Scirpts/Kyusang/StartMgr.cs
+++ Assets/02_Scirpts/Kyusang/StartMgr.cs
@@ -1,7 +1,13 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class StartMgr : MonoBehaviour
 {
+    private async void Start(){
+        await Task.Delay(3000);
+
+        SceneMgr.LoadLobby();
+    }
     public void GameStart()
     {
         SceneMgr.LoadLobby();
